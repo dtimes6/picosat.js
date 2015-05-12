f04a94b6ad1495708020fc527f2ba7bd5824d9a8
代鹏 <dtimes6@163.com>
2015-05-13 04:13:07 +0800

    Update picosat.js

--- picosat.js
+++ picosat.js
@@ -3,4 +3,16 @@ var s = new sat.PicoSAT();
 s.add_clause([1,-5,4]);
 s.add_clause([-1,5,3,4]);
 s.add_clause([-3,-4]);
-console.log(s.solve());
+
+var res;
+do {
+  if (res) {
+      console.log(res);
+      var n = [];
+      for(var i in res) {
+          n.push(-res[i]);
+      }
+      s.add_clause(n);
+  }
+  res = s.solve();
+} while(res instanceof Array);
